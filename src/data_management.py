cfd2870a7d94506e0920a9363766da53ac62ccf8
blackcatten sweden <m_arnesson@hotmail.com>
2024-06-05 16:53:33 +0000

    Add app.py, src, add and removed app_pages folder

---
+++ src/data_management.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import joblib
+
+
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def load_house_prices_data():
+    df = pd.read_csv("outputs/datasets/collection/HeritageHousing.csv")
+    return df
+
+
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def load_inherited_house_data():
+    df = pd.read_csv(
+        "inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/inherited_houses.csv")
+    return df
+
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
---
+++ src/machine_learning/predictive_analysis_ui.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+# Function predicts house prices using the regression pipeline
+
+
+def predict_sale_price(X_live, property_features, sale_price_pipeline):
+
+    # from live data, subset features related to this pipeline
+    # the features are filtered using the list of features from the pipeline
+    # this is to avoid a scilent fail in case the input features
+    # are not in the same order as in the dataset used to train the model.
+    X_live_sale_price = X_live.filter(property_features)
+
+    # predict
+    sale_price_prediction = sale_price_pipeline.predict(X_live_sale_price)
+
+    statement = (
+        f"* Given the features provided for the property, the model has "
+        f"  predicted a sale value of:"
+    )
+
+    # Format the value written to the page
+    # Formating learned from
+    # https://github.com/t-hullis/milestone-project-heritage-housing-issues/tree/main
+    if len(sale_price_prediction) == 1:
+        price = float(sale_price_prediction.round(1))
+        price = '${:,.2f}'.format(price)
+
+        st.write(statement)
+        st.write(f"**{price}**")
+    else:
+        st.write(
+            f"* Given the features provided for the inherited properties, "
+            f" the model has predicted sale values of:")
+        st.write(sale_price_prediction)
+
+    return sale_price_prediction
