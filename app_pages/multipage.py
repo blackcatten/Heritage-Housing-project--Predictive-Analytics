3209d3f0b095ba4b9c76786241fbbffb358a3ca2
blackcatten sweden <m_arnesson@hotmail.com>
2024-06-05 16:58:28 +0000

    Add multipage.py and page_summary.py

---
+++ app_pages/multipage.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+# Class to generate multiple Streamlit pages using an object oriented approach
+
+class MultiPage:
+
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+
+        st.set_page_config(
+            page_title=self.app_name,
+            page_icon="🖥️")  # You may add an icon, to personalize your App
+        # check links below for additional icons reference
+        # https://docs.streamlit.io/en/stable/api.html#streamlit.set_page_config
+        # https://twemoji.maxcdn.com/2/test/preview.html
+
+    def add_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        st.title(self.app_name)
+        page = st.sidebar.radio('Menu', self.pages,
+                                format_func=lambda page: page['title'])
+        page['function']()
