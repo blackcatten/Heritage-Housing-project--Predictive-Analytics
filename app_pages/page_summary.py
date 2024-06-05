32ebe1644bb4e4fb2a2bdd596723a41fc72178f0
blackcatten sweden <m_arnesson@hotmail.com>
2024-06-05 18:20:14 +0000

    Updated summary page

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -4,67 +4,61 @@ import pandas as pd
 
 def page_summary_body():
 
-    image_main = plt.imread(f"media/sale.jpg")
-    image_isu = plt.imread(f"media/sale.jpg")
+    image_main = plt.imread(f"media/sale.jpg")
+    image_isu = plt.imread(f"media/sale.jpg")
 
-    st.image(image_main, caption='Houses located in Ames, Iowa, USA')
+    st.image(image_main, caption='Houses located in Ames, Iowa, USA')
 
-    st.write("### Project Summary")
+    st.write("### Project Summary")
 
-    # text based on README file - "Dataset Content" section
-    st.info(
-        f"**Project Purpose and Motivation**\n\n"
-        f" The general pupose of this project is to provide a tool that allows"
-        f" a client to predict the potential sale price of a property in"
-        f" Ames, Iowa, by providing detailed and typical information on the"
-        f" real estate in question.\n\n"
-        f" Specifically, a client has requested this app in order"
-        f" to estimate the sale price for several inherited properties in "
-        f" Ames, Iowa. The client has provided a publically available data set"
-        f" which is used to train the machine learning model and "
-        f" predict local real estate sale prices. \n \n"
-        f"**Project Terminology**\n"
-        f"* A **client** is a person who uses this service or product.\n"
-        f"* The **sale price** is the estimated value of a home as it"
-        f" might be realized in a typical and unencumbered real estate"
-        f" transaction. \n"
-        f"* The home, the value of which is being estimated, may be refered to"
-        f" as **property, real estate, house, or home**. \n"
-        f"* The **features** or **attributes** of a home are characteristics"
-        f" used to describe the home. \n \n"
-        f"**Project Dataset**\n"
-        f"* The data set can be accessed at "
-        f"[Kaggle](https://www.kaggle.com/datasets/codeinstitute/housing-prices-data)"  # noqa
-        f" where it is hosted by Code Institute.\n"
-        f"* The dataset represents a record of approx. 1500 real estate "
-        f" sales in Ames, Iowa. Each record contains 23 features indicating"
-        f" the house profile, such as Floor Area, Basement, Garage, "
-        f" Kitchen, Lot,"
-        f" Porch, Wood Deck, and Year Built. It also contains the Sale Price."
-        f" The features are extensive, so please visit the site for more"
-        f" information.")
+    st.info(
+        f"**Project Purpose and Motivation**\n\n"
+        f" The primary purpose of this project is to create a tool that "
+        f"enables clients to predict the potential sale price of properties"
+        f" in Ames, Iowa, by providing detailed and typical information about"
+        f" the real estate in question. Specifically, a client has requested"
+        f" this app to estimate the sale prices for several inherited properties "
+        f"in Ames, Iowa. The client has supplied a publicly available dataset, "
+        f"which is used to train the machine learning model to predict local "
+        f"real estate sale prices."
+        f"**Project Terminology**\n\n"
+        f"- **Client**: A person who uses this service or product."
+        f"- **Sale Price**: The estimated value of a home as it"
+        f" might be realized in a typical and unencumbered real estate transaction."
+        f"- **Property/Real Estate/House/Home**: Terms used interchangeably to"
+        f" refer to the home whose value is being estimated."
+        f"- **Features/Attributes**: Characteristics used to describe the home."
+        f"**Project Dataset**"
+        f"- The dataset can be accessed at"
+        f" [Kaggle](https://www.kaggle.com/datasets/codeinstitute/housing-prices-data)"
+        f" where it is hosted by Code Institute."
+        f"- The dataset includes records of approximately 1,500 real estate sales"
+        f" in Ames, Iowa. Each record contains 23 features detailing the house profile,"
+        f"such as Floor Area, Basement, Garage, Kitchen, Lot, Porch, Wood Deck, and Year"
+        f" Built. It also includes the Sale Price. The dataset's features are extensive,"
+        f" and more information can be found on the Kaggle site."
+    )
 
-    # copied from README file - "Business Requirements" section
-    st.success(
-        f"**Business Requirements**\n\n"
-        f"The project has 3 business requirements:\n"
-        f"* 1. The client is interested in understanding the correlation "
-        f" between a properties attributes/features and the sale price."
-        f" Therefore, the client expects data visualization of the correlated"
-        f" variables against the sale prices for illustration. \n"
-        f"* 2. The client is interested in predicting the potential sale "
-        f" prices"
-        f" for properties in Ames, Iowa, and specifically, she wants to"
-        f" determine a potential value for the properties she inherited. \n"
-        f"* 3. The client would like to access the outcomes easily using"
-        f" an online application."
-    )
+    st.success(
+        f"**Business Requirements**\n\n"
+        f"The project has 3 business requirements:\n"
+        f"1. **Correlation Study**: The client is interested"
+        f" in understanding the correlation between a property's "
+        f"attributes/features and the sale price. Therefore, the"
+        f"client expects data visualization of the correlated "
+        f"variables against the sale prices for illustration."
+        f"2. **Sale Price Prediction**: The client wants to predict"
+        f" potential sale prices for properties in Ames, Iowa, and "
+        f"specifically determine the potential value of the "
+        f"properties she inherited."
+        f"3. **Online Application**: The client would like to access"
+        f" the outcomes easily through an online application."
+    )
 
-    # Link to README file, so the users can have access to full
-    # project documentation
-    st.write(
-        f"* For additional information on this project please consult the "
-        f"[README](https://github.com/blackcatten/Heritage-Housing-project--Predictive-Analytics/tree/main)")
+    # Link to README file
 
-   
+    st.write(
+        f"* For additional information on this project please consult the "
+        f"[README](https://github.com/blackcatten/Heritage-Housing-project--Predictive-Analytics/tree/main)")
+
 
