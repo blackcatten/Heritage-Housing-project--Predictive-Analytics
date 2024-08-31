05494b849a8c6853f7d3f962c0467d5d91fe9f9b
Mikael Arnesson <m_arnesson@hotmail.com>
2024-08-31 08:56:41 +0000

    Updated data management with inherited houses

--- src/data_management.py
+++ src/data_management.py
@@ -13,7 +13,7 @@ def load_house_prices_data():
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
 def load_inherited_house_data():
     df = pd.read_csv(
-        "inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/InheritedHouses.csv")
+        "inputs/datasets/raw/house-price-20211124T154130Z-001/house-price/inherited_houses.csv")
     return df
 
 
